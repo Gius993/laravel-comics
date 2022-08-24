dbf021dc0e6f7b0a579593fb112d326102edbfa7
Gius993 <giuseppevidili9@gmail.com>
2022-08-24 18:30:21 +0200

    da finire

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
 	<section>
 		<div class="container">
 			<div class="row">
+				@foreach ($comic_array as $item)
+					
 				<div class="card">
 					<div class="image">
 						<img src="https://media-assets.wired.it/photos/615f453651b7b2945256d47a/1:1/w_354%2Cc_limit/faq-poster.6a05e15.jpg" alt="">
@@ -11,6 +13,7 @@
 						<h3>action comics</h3>
 					</div>
 				</div>
+				@endforeach
 				
 			</div>
 		</div>
--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 
 
-Route::get('/card', function () {
+Route::get('/home', function () {
     $comic_array = config('comics');
-    dd($comic_array);
-  
-    return view('card');
+    
+    return view('home', $data);
 });
