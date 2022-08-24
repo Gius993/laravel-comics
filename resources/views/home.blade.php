204a8cd706003552aa342173dc6866c9e2f023f8
Gius993 <giuseppevidili9@gmail.com>
2022-08-24 17:50:15 +0200

    card interne

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,14 @@
 		<div class="container">
 			<div class="row">
 				<div class="card">
-					
+					<div class="image">
+						<img src="https://media-assets.wired.it/photos/615f453651b7b2945256d47a/1:1/w_354%2Cc_limit/faq-poster.6a05e15.jpg" alt="">
+					</div>
+					<div class="texts">
+						<h3>action comics</h3>
+					</div>
 				</div>
+				
 			</div>
 		</div>
 	</section>
