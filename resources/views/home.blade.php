d7b57035f2c69a34f97f0ad5e8c7a4a806091d66
Gius993 <giuseppevidili9@gmail.com>
2022-08-24 17:42:14 +0200

    card e creazione main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('main')
-	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="card">
+					
+				</div>
+			</div>
+		</div>
+	</section>
 @endsection
